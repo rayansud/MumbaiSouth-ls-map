f74518cac76838f254daf865b20079798dc61fc7
Rayan <rayansud@users.noreply.github.com>
2023-11-03 13:28:23 +0530

    Updated all with 2019 locs, cleaner viz, better file structure

--- 2014map/mapbox/style.js
+++ 2014map/mapbox/style.js
@@ -12,14 +12,14 @@ var styleJSON = {
             "tiles": ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
             "tileSize": 256
         },
-        "Consolidated2014LSwithboothlocs_1": {
+        "Merged_1": {
             "type": "geojson",
-            "data": json_Consolidated2014LSwithboothlocs_1
+            "data": json_Merged_1
         }
                     ,
-        "Clipped_2": {
+        "ClippedVoronoi_2": {
             "type": "geojson",
-            "data": json_Clipped_2
+            "data": json_ClippedVoronoi_2
         }
                     },
     "sprite": "",
@@ -39,19 +39,19 @@ var styleJSON = {
             "source": "OpenStreetMap_0"
         },
         {
-            "id": "lyr_Consolidated2014LSwithboothlocs_1_0",
+            "id": "lyr_Merged_1_0",
             "type": "circle",
-            "source": "Consolidated2014LSwithboothlocs_1",
+            "source": "Merged_1",
             "layout": {},
-            "paint": {'circle-radius': ['/', 7.142857142857142, 2], 'circle-color': '#91522d', 'circle-opacity': 1.0, 'circle-stroke-width': 1, 'circle-stroke-color': '#232323'}
+            "paint": {'circle-radius': ['/', 7.142857142857142, 2], 'circle-color': '#becf50', 'circle-opacity': 1.0, 'circle-stroke-width': 1, 'circle-stroke-color': '#232323'}
         }
 ,
         {
-            "id": "lyr_Clipped_2_0",
+            "id": "lyr_ClippedVoronoi_2_0",
             "type": "fill",
-            "source": "Clipped_2",
+            "source": "ClippedVoronoi_2",
             "layout": {},
-            "paint": {'fill-opacity': ['case', ['all', ['>=', ['get', 'LossMargin'], ['-', 0, 0.915194]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.621127]]], 0.8, ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.621127]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.465845]]], 0.8, ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.465845]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.310563]]], 0.8, ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.310563]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.155282]]], 0.8, ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.155282]], ['<=', ['get', 'LossMargin'], 0.0]], 0.8, ['all', ['>', ['get', 'LossMargin'], 0.0], ['<=', ['get', 'LossMargin'], 0.155282]], 0.8, ['all', ['>', ['get', 'LossMargin'], 0.155282], ['<=', ['get', 'LossMargin'], 0.310563]], 0.8, ['all', ['>', ['get', 'LossMargin'], 0.310563], ['<=', ['get', 'LossMargin'], 0.465845]], 0.8, ['all', ['>', ['get', 'LossMargin'], 0.465845], ['<=', ['get', 'LossMargin'], 0.621127]], 0.8, ['all', ['>', ['get', 'LossMargin'], 0.621127], ['<=', ['get', 'LossMargin'], 0.776408]], 0.8, 0], 'fill-color': ['case', ['all', ['>=', ['get', 'LossMargin'], ['-', 0, 0.915194]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.621127]]], '#0b0405', ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.621127]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.465845]]], '#28192f', ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.465845]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.310563]]], '#3b2f5f', ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.310563]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.155282]]], '#40498e', ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.155282]], ['<=', ['get', 'LossMargin'], 0.0]], '#366b9f', ['all', ['>', ['get', 'LossMargin'], 0.0], ['<=', ['get', 'LossMargin'], 0.155282]], '#348ba6', ['all', ['>', ['get', 'LossMargin'], 0.155282], ['<=', ['get', 'LossMargin'], 0.310563]], '#39abac', ['all', ['>', ['get', 'LossMargin'], 0.310563], ['<=', ['get', 'LossMargin'], 0.465845]], '#55caad', ['all', ['>', ['get', 'LossMargin'], 0.465845], ['<=', ['get', 'LossMargin'], 0.621127]], '#a3e0ba', ['all', ['>', ['get', 'LossMargin'], 0.621127], ['<=', ['get', 'LossMargin'], 0.776408]], '#def5e5', '#ffffff']}
+            "paint": {'fill-opacity': ['case', ['all', ['>=', ['get', 'LossMargin'], ['-', 0, 0.915194]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.621127]]], 1.0, ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.621127]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.465845]]], 1.0, ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.465845]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.310563]]], 1.0, ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.310563]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.155282]]], 1.0, ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.155282]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.0]]], 1.0, ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.0]], ['<=', ['get', 'LossMargin'], 0.155282]], 1.0, ['all', ['>', ['get', 'LossMargin'], 0.155282], ['<=', ['get', 'LossMargin'], 0.310563]], 1.0, ['all', ['>', ['get', 'LossMargin'], 0.310563], ['<=', ['get', 'LossMargin'], 0.465845]], 1.0, ['all', ['>', ['get', 'LossMargin'], 0.465845], ['<=', ['get', 'LossMargin'], 0.621127]], 1.0, ['all', ['>', ['get', 'LossMargin'], 0.621127], ['<=', ['get', 'LossMargin'], 0.776408]], 1.0, 0], 'fill-color': ['case', ['all', ['>=', ['get', 'LossMargin'], ['-', 0, 0.915194]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.621127]]], '#d7191c', ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.621127]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.465845]]], '#e85b3b', ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.465845]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.310563]]], '#f99d59', ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.310563]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.155282]]], '#fec980', ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.155282]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.0]]], '#ffedaa', ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.0]], ['<=', ['get', 'LossMargin'], 0.155282]], '#ecf7b9', ['all', ['>', ['get', 'LossMargin'], 0.155282], ['<=', ['get', 'LossMargin'], 0.310563]], '#c7e8ad', ['all', ['>', ['get', 'LossMargin'], 0.310563], ['<=', ['get', 'LossMargin'], 0.465845]], '#9dd3a6', ['all', ['>', ['get', 'LossMargin'], 0.465845], ['<=', ['get', 'LossMargin'], 0.621127]], '#64abb0', ['all', ['>', ['get', 'LossMargin'], 0.621127], ['<=', ['get', 'LossMargin'], 0.776408]], '#2b83ba', '#ffffff']}
         }
 ],
 }
--- 2019map/mapbox/style.js
+++ 2019map/mapbox/style.js
@@ -12,14 +12,14 @@ var styleJSON = {
             "tiles": ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
             "tileSize": 256
         },
-        "Consolidated2019LSwithboothlocsnotcleaned_1": {
+        "Merged_1": {
             "type": "geojson",
-            "data": json_Consolidated2019LSwithboothlocsnotcleaned_1
+            "data": json_Merged_1
         }
                     ,
-        "Clipped_2": {
+        "ClippedVoronoiclipped_2": {
             "type": "geojson",
-            "data": json_Clipped_2
+            "data": json_ClippedVoronoiclipped_2
         }
                     },
     "sprite": "",
@@ -39,19 +39,19 @@ var styleJSON = {
             "source": "OpenStreetMap_0"
         },
         {
-            "id": "lyr_Consolidated2019LSwithboothlocsnotcleaned_1_0",
+            "id": "lyr_Merged_1_0",
             "type": "circle",
-            "source": "Consolidated2019LSwithboothlocsnotcleaned_1",
+            "source": "Merged_1",
             "layout": {},
-            "paint": {'circle-radius': ['/', 7.142857142857142, 2], 'circle-color': '#b7484b', 'circle-opacity': 1.0, 'circle-stroke-width': 1, 'circle-stroke-color': '#232323'}
+            "paint": {'circle-radius': ['/', 7.142857142857142, 2], 'circle-color': '#f3a6b2', 'circle-opacity': 1.0, 'circle-stroke-width': 1, 'circle-stroke-color': '#232323'}
         }
 ,
         {
-            "id": "lyr_Clipped_2_0",
+            "id": "lyr_ClippedVoronoiclipped_2_0",
             "type": "fill",
-            "source": "Clipped_2",
+            "source": "ClippedVoronoiclipped_2",
             "layout": {},
-            "paint": {'fill-opacity': ['case', ['all', ['>=', ['get', 'LossMargin'], ['-', 0, 0.98556]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.603642]]], 0.8, ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.603642]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.452732]]], 0.8, ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.452732]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.301821]]], 0.8, ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.301821]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.150911]]], 0.8, ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.150911]], ['<=', ['get', 'LossMargin'], 0.0]], 0.8, ['all', ['>', ['get', 'LossMargin'], 0.0], ['<=', ['get', 'LossMargin'], 0.150911]], 0.8, ['all', ['>', ['get', 'LossMargin'], 0.150911], ['<=', ['get', 'LossMargin'], 0.301821]], 0.8, ['all', ['>', ['get', 'LossMargin'], 0.301821], ['<=', ['get', 'LossMargin'], 0.452732]], 0.8, ['all', ['>', ['get', 'LossMargin'], 0.452732], ['<=', ['get', 'LossMargin'], 0.603642]], 0.8, ['all', ['>', ['get', 'LossMargin'], 0.603642], ['<=', ['get', 'LossMargin'], 0.754553]], 0.8, 0], 'fill-color': ['case', ['all', ['>=', ['get', 'LossMargin'], ['-', 0, 0.98556]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.603642]]], '#0b0405', ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.603642]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.452732]]], '#28192f', ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.452732]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.301821]]], '#3b2f5f', ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.301821]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.150911]]], '#40498e', ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.150911]], ['<=', ['get', 'LossMargin'], 0.0]], '#366b9f', ['all', ['>', ['get', 'LossMargin'], 0.0], ['<=', ['get', 'LossMargin'], 0.150911]], '#348ba6', ['all', ['>', ['get', 'LossMargin'], 0.150911], ['<=', ['get', 'LossMargin'], 0.301821]], '#39abac', ['all', ['>', ['get', 'LossMargin'], 0.301821], ['<=', ['get', 'LossMargin'], 0.452732]], '#55caad', ['all', ['>', ['get', 'LossMargin'], 0.452732], ['<=', ['get', 'LossMargin'], 0.603642]], '#a3e0ba', ['all', ['>', ['get', 'LossMargin'], 0.603642], ['<=', ['get', 'LossMargin'], 0.754553]], '#def5e5', '#ffffff']}
+            "paint": {'fill-opacity': ['case', ['all', ['>=', ['get', 'LossMargin'], ['-', 0, 0.98556]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.660234]]], 1.0, ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.660234]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.565914]]], 1.0, ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.565914]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.471595]]], 1.0, ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.471595]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.377276]]], 1.0, ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.377276]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.282957]]], 1.0, ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.282957]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.188638]]], 1.0, ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.188638]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.094319]]], 1.0, ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.094319]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.0]]], 1.0, ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.0]], ['<=', ['get', 'LossMargin'], 0.094319]], 1.0, ['all', ['>', ['get', 'LossMargin'], 0.094319], ['<=', ['get', 'LossMargin'], 0.188638]], 1.0, ['all', ['>', ['get', 'LossMargin'], 0.188638], ['<=', ['get', 'LossMargin'], 0.282957]], 1.0, ['all', ['>', ['get', 'LossMargin'], 0.282957], ['<=', ['get', 'LossMargin'], 0.377276]], 1.0, ['all', ['>', ['get', 'LossMargin'], 0.377276], ['<=', ['get', 'LossMargin'], 0.471595]], 1.0, ['all', ['>', ['get', 'LossMargin'], 0.471595], ['<=', ['get', 'LossMargin'], 0.565914]], 1.0, ['all', ['>', ['get', 'LossMargin'], 0.565914], ['<=', ['get', 'LossMargin'], 0.660234]], 1.0, ['all', ['>', ['get', 'LossMargin'], 0.660234], ['<=', ['get', 'LossMargin'], 0.754553]], 1.0, 0], 'fill-color': ['case', ['all', ['>=', ['get', 'LossMargin'], ['-', 0, 0.98556]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.660234]]], '#d7191c', ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.660234]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.565914]]], '#e1412e', ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.565914]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.471595]]], '#eb6841', ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.471595]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.377276]]], '#f59053', ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.377276]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.282957]]], '#fdb367', ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.282957]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.188638]]], '#fec980', ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.188638]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.094319]]], '#fedf99', ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.094319]], ['<=', ['get', 'LossMargin'], ['-', 0, 0.0]]], '#fff4b2', ['all', ['>', ['get', 'LossMargin'], ['-', 0, 0.0]], ['<=', ['get', 'LossMargin'], 0.094319]], '#f4fabb', ['all', ['>', ['get', 'LossMargin'], 0.094319], ['<=', ['get', 'LossMargin'], 0.188638]], '#ddf1b4', ['all', ['>', ['get', 'LossMargin'], 0.188638], ['<=', ['get', 'LossMargin'], 0.282957]], '#c7e8ad', ['all', ['>', ['get', 'LossMargin'], 0.282957], ['<=', ['get', 'LossMargin'], 0.377276]], '#b1dfa6', ['all', ['>', ['get', 'LossMargin'], 0.377276], ['<=', ['get', 'LossMargin'], 0.471595]], '#91cba8', ['all', ['>', ['get', 'LossMargin'], 0.471595], ['<=', ['get', 'LossMargin'], 0.565914]], '#6fb3ae', ['all', ['>', ['get', 'LossMargin'], 0.565914], ['<=', ['get', 'LossMargin'], 0.660234]], '#4d9bb4', ['all', ['>', ['get', 'LossMargin'], 0.660234], ['<=', ['get', 'LossMargin'], 0.754553]], '#2b83ba', '#ffffff']}
         }
 ],
 }
